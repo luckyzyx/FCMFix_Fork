0dd5a44b732e5b11dae91d563ac297e073cfedad
luckyzyx <2813467705@qq.com>
2025-03-21 22:21:23 +0800

    修复BroadCast自启

--- app/src/main/java/com/luckyzyx/fcmfix/hook/scopes/android/AutoStartFix.kt
+++ app/src/main/java/com/luckyzyx/fcmfix/hook/scopes/android/AutoStartFix.kt
@@ -8,8 +8,6 @@ import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.current
 import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
-import com.highcapable.yukihookapi.hook.type.android.IntentClass
-import com.highcapable.yukihookapi.hook.type.android.ResolveInfoClass
 import com.highcapable.yukihookapi.hook.type.java.BooleanType
 import com.luckyzyx.fcmfix.hook.HookUtils.isAllowPackage
 import com.luckyzyx.fcmfix.hook.HookUtils.isFCMIntent
@@ -39,10 +37,12 @@ object AutoStartFix : YukiBaseHooker() {
                     val context = ams.current().field {
                         name = "mContext";superClass()
                     }.cast<Context>() ?: return@after
-                    val intent = args(args.indexOf(IntentClass)).cast<Intent>()
+                    val intentIndex = args.indexOfFirst { it is Intent }.takeIf { it != -1 }
                         ?: return@after
-                    val resolveInfo = args(args.indexOf(ResolveInfoClass)).cast<ResolveInfo>()
+                    val resolveIndex = args.indexOfFirst { it is ResolveInfo }.takeIf { it != -1 }
                         ?: return@after
+                    val intent = args(intentIndex).cast<Intent>() ?: return@after
+                    val resolveInfo = args(resolveIndex).cast<ResolveInfo>() ?: return@after
                     val packName = resolveInfo.activityInfo.applicationInfo.packageName
                     if (isFCMIntent(intent) && isAllowPackage(allowList, packName)) {
                         context.sendGsmLogBroadcast("[$packName] Allow Start From BroadCast")
